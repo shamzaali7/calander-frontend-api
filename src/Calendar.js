6ab0b3c7d99f5a00366eee0be36b2622cc42de7e
bd6981 <brittanyjdavis22@gmail.com>
2022-12-04 11:21:53 -0500

    working on component.js

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,17 +1,75 @@
 import React, { Component } from "react";
-import { Calendar, momentLocalizer } from "react-big-calendar";
+import { BigCalendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import { toDate } from "date-fns";
-import React from 'react'
 
-const Calendar = () => {
-  return (
-    <div>Calendar</div>
-  )
+
+
+BigCalendar.momentLocalizer(moment);
+
+class Calendar extends Component {
+  constructor() {
+    super();
+    this.state = {
+      events: [],
+      title: "",
+      start: "",
+      end: "",
+      desc: "",
+      openSlot: false,
+      openEvent: false,
+      clickedEvent: {}
+    };
+    this.handleClose = this.handleClose.bind(this);
+  }
+
+  handleClose() {
+    this.setState({ openEvent: false, openSlot: false });
+  }
+
+  //  Allows user to click on calendar and see existing task
+  handleSlotSelected(slotInfo) {
+    console.log("Real slotInfo", slotInfo);
+    this.setState({
+      title: "",
+      desc: "",
+      start: slotInfo.start,
+      end: slotInfo.end,
+      openSlot: true
+    });
+  }
+
+  handleEventSelected(event) {
+    console.log("event", event);
+    this.setState({
+      openEvent: true,
+      clickedEvent: event,
+      start: event.start,
+      end: event.end,
+      title: event.title,
+      desc: event.desc
+    });
+  }
 }
 
-export default Calendar
+    return (
+      <div id="Calendar">
+        {/* react-big-calendar library utilized to render calendar*/}
+        <BigCalendar
+          events={this.state.events}
+          views={["month"]}
+          timeslots={2}
+          defaultView="month"
+          defaultDate={new Date()}
+          selectable={true}
+          onSelectEvent={event => this.handleEventSelected(event)}
+          onSelectSlot={slotInfo => this.handleSlotSelected(slotInfo)}
+        />
 
-const localizer = momentLocalizer(moment);
-
+        {/* Material-ui Modal for booking new appointment */}
+        
+      </div>
+    );
+    
+export default Calendar;
