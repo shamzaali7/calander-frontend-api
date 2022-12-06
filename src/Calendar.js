50926b0d5a4c97b492883d1840545775fe2ba06c
bd6981 <brittanyjdavis22@gmail.com>
2022-12-05 21:23:06 -0500

    fixed modal

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,61 +1,66 @@
 import React, {
     Component
 } from "react";
-import Modal from './Modal'
+import MyModal from './Modal'
 import "./App.css"
+
 const Calendar = () => {
 
 
+    
     return (
-        <div>
-        <div class="divTable blueTable">
+        <div className="calendar" >
+        
+        <div className="divTable blueTable">
         <div class="divTableBody">
         <div class="divTableRow">
-        <div class="divTableCell">1<Modal><button/></Modal></div>
-        <div class="divTableCell">2<Modal><button/></Modal></div>
-        <div class="divTableCell">3<Modal><button/></Modal></div>
-        <div class="divTableCell">4<Modal><button/></Modal></div>
-        <div class="divTableCell">5<Modal><button/></Modal></div>
-        <div class="divTableCell">6<Modal><button/></Modal></div>
-        <div class="divTableCell">7<Modal><button/></Modal></div>
+        <div class="divTableCell">1<MyModal><button/></MyModal></div>
+        <div class="divTableCell">2<MyModal><button/></MyModal></div>
+        <div class="divTableCell">3<MyModal><button/></MyModal></div>
+        <div class="divTableCell">4<MyModal><button/></MyModal></div>
+        <div class="divTableCell">5<MyModal><button/></MyModal></div>
+        <div class="divTableCell">6<MyModal><button/></MyModal></div>
+        <div class="divTableCell">7<MyModal><button/></MyModal></div>
         </div>
         <div class="divTableRow">
-        <div class="divTableCell">8<Modal><button/></Modal></div>
-        <div class="divTableCell">9<Modal><button/></Modal></div>
-        <div class="divTableCell">10<Modal><button/></Modal></div>
-        <div class="divTableCell">11<Modal><button/></Modal></div>
-        <div class="divTableCell">12<Modal><button/></Modal></div>
-        <div class="divTableCell">13<Modal><button/></Modal></div>
-        <div class="divTableCell">14<Modal><button/></Modal></div>
+        <div class="divTableCell">8<MyModal><button/></MyModal></div>
+        <div class="divTableCell">9<MyModal><button/></MyModal></div>
+        <div class="divTableCell">10<MyModal><button/></MyModal></div>
+        <div class="divTableCell">11<MyModal><button/></MyModal></div>
+        <div class="divTableCell">12<MyModal><button/></MyModal></div>
+        <div class="divTableCell">13<MyModal><button/></MyModal></div>
+        <div class="divTableCell">14<MyModal><button/></MyModal></div>
         </div>
         <div class="divTableRow">
-        <div class="divTableCell">15<Modal><button/></Modal></div>
-        <div class="divTableCell">16<Modal><button/></Modal></div>
-        <div class="divTableCell">17<Modal><button/></Modal></div>
-        <div class="divTableCell">18<Modal><button/></Modal></div>
-        <div class="divTableCell">19<Modal><button/></Modal></div>
-        <div class="divTableCell">20<Modal><button/></Modal></div>
-        <div class="divTableCell">21<Modal><button/></Modal></div>
+        <div class="divTableCell">15<MyModal><button/></MyModal></div>
+        <div class="divTableCell">16<MyModal><button/></MyModal></div>
+        <div class="divTableCell">17<MyModal><button/></MyModal></div>
+        <div class="divTableCell">18<MyModal><button/></MyModal></div>
+        <div class="divTableCell">19<MyModal><button/></MyModal></div>
+        <div class="divTableCell">20<MyModal><button/></MyModal></div>
+        <div class="divTableCell">21<MyModal><button/></MyModal></div>
         </div>
          <div class="divTableRow">
-        <div class="divTableCell">22<Modal><button/></Modal></div>
-        <div class="divTableCell">23<Modal><button/></Modal></div>
-        <div class="divTableCell">24<Modal><button/></Modal></div>
-        <div class="divTableCell">25<Modal><button/></Modal></div>
-        <div class="divTableCell">26<Modal><button/></Modal></div>
-        <div class="divTableCell">27<Modal><button/></Modal></div>
-        <div class="divTableCell">28<Modal><button/></Modal></div>
+        <div class="divTableCell">22<MyModal><button/></MyModal></div>
+        <div class="divTableCell">23<MyModal><button/></MyModal></div>
+        <div class="divTableCell">24<MyModal><button/></MyModal></div>
+        <div class="divTableCell">25<MyModal><button/></MyModal></div>
+        <div class="divTableCell">26<MyModal><button/></MyModal></div>
+        <div class="divTableCell">27<MyModal><button/></MyModal></div>
+        <div class="divTableCell">28<MyModal><button/></MyModal></div>
         </div>
         <div class="divTableRow">
-        <div class="divTableCell">29<Modal><button/></Modal></div>
-        <div class="divTableCell">30<Modal><button/></Modal></div>
-        <div class="divTableCell">31<Modal><button/></Modal></div>
-        </div>
+        <div class="divTableCell">29<MyModal><button/></MyModal></div>
+        <div class="divTableCell">30<MyModal><button/></MyModal></div>
+        <div class="divTableCell">31<MyModal><button/></MyModal></div>
+        </div> 
         </div>
        </div>
-       </div>
+      
+      
+        </div>
 
-    )
-}
+    )}
+
 
 export default Calendar
--- src/Modal.js
+++ src/Modal.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
 import Button from '@mui/material/Button';
-import Calendar from './Calendar'
+
 import './App.css'
 
 // https://mui.com/material-ui/react-modal/
@@ -22,49 +22,80 @@ const style = {
 };
 
 export default function MyModal() {
-    // const [open, setOpen] = useState([false]);
-    // const handleOpen = () => {
-    //     setOpen(true);
-    // }
-    // // console.log(open)
-    // const handleClose = () => {
-    //     setOpen(false);
-    // }
+   
    
       const [open, setOpen] = useState(false);
       const handleOpen = () => setOpen(true);
       const handleClose = () => setOpen(false);
-
-    const [task, setTask] = useState([]);
+      const [taskValue, setTaskValue] = useState("");
+      const [taskList, setTaskList] = useState([]);
     const handlInputChange = (event) => {
-        setTask(event.target.value);
+        setTaskValue(event.target.value);
 
     }
     const handleSubmit = (event) => {
         event.preventDefault();
-        setTask(event.target.value);
+        
+      setTaskList(prevTaskValue =>
 
-        setTask(prevTask =>
-          prevTask.concat({ value: task })
-        );
-        setTask("");
-    }
+        prevTaskValue.concat({ value: taskValue })
+      );
+      setTaskValue("");
+    };
+    
+    
+    const handleCheckBoxClick = (listnumber, event) => {
+      setTaskList(taskList.map(
+          (item, index) =>
+            {return{
+              ...item,
+              isChecked: index === listnumber ? event.currentTarget.checked : item.isChecked
+            }}
+        
+      ))}
+          
     return (
         <div>
-      <Button onClick={handleOpen}>+</Button>
+      <Button onClick={handleOpen}>add</Button>
       <Modal
         open={open}
         onClose={handleClose}
         aria-labelledby="parent-modal-title"
         form={"input"}
-        inputType="text"
-        onSubmit={handleSubmit}
-        data={'displayText'}>
+        type="text"
+        onSubmit={handleSubmit}>
          <Box sx={{ ...style, width: 200 }}>
           <h2 id="parent-modal-title">Tasks</h2>
-          <form onSubmit={handleSubmit}><input type="text" onChange={handlInputChange} /><button>save</button></form>
-        </Box> 
+          <form onSubmit={handleSubmit}>
+            <input type="text" value={taskValue} onChange={handlInputChange} />
+            <button onClick={handleSubmit}>save</button>
+          </form>
+        </Box>
       </Modal>
+      <div>
+          
+          {taskList.map(
+            (item, index) =>
+              item.value && (
+                <li>
+                 {/* after  */}
+                  <input
+                  
+                    type="checkbox"
+                    id='btn'
+                    onClick={handleCheckBoxClick.bind(null, index)}
+                    className="strikethrough"
+                  />
+                  {item.value}
+                  {item.isChecked}
+                </li>
+              )
+          )}
+        </div> 
+          
+           
+    
+      
     </div>
     );
 }
